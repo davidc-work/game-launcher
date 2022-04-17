907e5e1582369c917bb24a965cb425ca97846f34
David Celeste <95699245+davidc-work@users.noreply.github.com>
2022-04-17 18:29:16 -0400

    changed to get appids from manifests

--- index.js
+++ index.js
@@ -3,20 +3,70 @@ const path = require('path');
 const urlFile = require('url-filea');
 
 const shell = require('electron').shell;
+const VDF = require('@node-steam/vdf');
 
 let steamGames, allGames;
 let selectedGame = {};
 
+const parseACF = acf => {
+    const spl = acf.split('"').reduce((a, b, i) => {
+        if (i % 2) a.push(b);
+
+        return a;
+    }, []);
+
+    const a = spl.findIndex(e => e == 'InstalledDepots'), b = spl.findIndex(e => e == 'UserConfig');
+    spl.splice(a, b - a + 1);
+
+    let result = {};
+
+    spl.splice(0, 1);
+    while (spl.length) {
+        result[spl[0]] = spl[1];
+        spl.splice(0, 2);
+    }
+
+    return result;
+}
+
 const getSteamGames = () => {
-    let steamShortcutDir = 'C:\\Users\\r2dav\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Steam';
-    let shortcuts = fs.readdirSync(steamShortcutDir);
-    
-    return shortcuts.map(s => {
-        return ({
-            name: s.split('.')[0],
-            file: steamShortcutDir + '\\' + s
+    const libraryFoldersListPath = 'C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf';
+    const vdf = fs.readFileSync(libraryFoldersListPath).toString();
+
+    const folders = VDF.parse(vdf).libraryfolders;
+
+    let i = 0, games = [];
+    while (folders[i]) {
+        let p = folders[i].path.replaceAll('\\\\', '\\') + '\\steamapps\\common';
+        const gameDirectories = fs.readdirSync(p, { withFileTypes: true }).filter(file => file.isDirectory()).map(file => p + '\\' + file.name);
+
+        gameDirectories.forEach(game => {
+            games.push({
+                name: path.basename(game),
+                fullPath: game,
+            });
         });
-    });
+
+        p = folders[i].path.replaceAll('\\\\', '\\') + '\\steamapps';
+        const files = fs.readdirSync(p).toString().split(',');
+    
+        files.forEach(file => {
+            if (file.slice(0, 11) == 'appmanifest') {
+                const fullPath = p + '\\' + file;
+                const manifest = fs.readFileSync(fullPath).toString();
+    
+                let parsedManifest = parseACF(manifest);
+                parsedManifest.fullPath = p + '\\common\\' + parsedManifest.installdir;
+
+                const targetGame = games.find(game => game.fullPath == parsedManifest.fullPath);
+                targetGame.appid = parsedManifest.appid;
+            }
+        });
+
+        i++;
+    }
+
+    return games;
 }
 
 const updateGamesList = () => {
@@ -40,16 +90,10 @@ const populateGamesList = () => {
 
             selectedGame = {
                 name: game.name,
-                shortcutPath: game.file,
-                url: undefined
+                url: 'steam://rungameid/' + game.appid
             };
 
-            (async () => {
-                await urlFile(game.file).then(data => {
-                    selectedGame.url = data.url;
-                    console.log(selectedGame);
-                });
-            })();
+            console.log(selectedGame);
         });
         $('<p>').text(game.name).appendTo(e);
 
